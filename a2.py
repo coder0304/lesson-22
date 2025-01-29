bc5cbeddaca8b65e00f1709b8435de6a6aa95dcd
coder0304 <nellikatgowri@gmail.com>
2025-01-29 19:03:56 +0530

    a2.py

---
+++ a2.py
@@ -0,0 +1,8 @@
+setx={"green","blue"}
+sety={"green","yelow"}
+print("og set element:")
+print(setx)
+print(sety)
+print("\nIntersection of two said sets:")
+setz=setx.intersection(sety)
+print(setz)
---
+++ a3.py
@@ -0,0 +1,8 @@
+setx={"green","blue"}
+sety={"green","yelow"}
+print("og set element:")
+print(setx)
+print(sety)
+print("\nUnion of two said sets:")
+setz=setx.union(sety)
+print(setz)
